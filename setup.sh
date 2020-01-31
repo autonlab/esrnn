40e15dc78bd004a10f7f82a87b259499732882d7
cristianchallu <cristiani.challu@gmail.com>
2020-01-31 11:04:54 -0500

    torch version

--- setup.sh
+++ setup.sh
@@ -6,20 +6,13 @@ conda activate d3m_esrnn
 # basic
 conda install -c anaconda numpy==1.16.1
 conda install -c anaconda pandas==0.25.2
-conda install -c anaconda scipy==1.2.1
-conda install -c anaconda scikit-learn==0.21.3
 
 # visualization
 conda install -c conda-forge matplotlib==3.1.1
-conda install -c anaconda seaborn==0.9.0
 
-# dynet
-conda install -c anaconda cython
-conda install -c intel mkl
-pip install dynet
+# pytorch
+conda install pytorch=1.3.1 -c pytorch
 
 conda install -c conda-forge jupyterlab
 ipython kernel install --user --name=d3m_esrnn
-conda install -c anaconda pylint
-conda install -c anaconda pyyaml
 conda deactivate
