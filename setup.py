07d8b91679d532b50db06ba3deaaea528a7894ad
Remi Rampin <remi@rampin.org>
2021-03-18 15:34:19 -0400

    Add license metadata to PyPI package

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
     description='Auton Lab TA1 primitives',
     author=read_package_variable('__author__'),
     author_email=read_package_variable('__author_email__'),
+    license='MIT',
     packages=find_packages(exclude=['docs', 'tests*']),
     install_requires=[
         'd3m',
